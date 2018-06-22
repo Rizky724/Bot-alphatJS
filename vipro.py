a7082ebd6cd81b43c6da291ee32d1f663be8de01
Rizky724 <39096317+Rizky724@users.noreply.github.com>
2018-06-22 14:09:09 +0700

    Add files via upload

--- vipro.py
+++ vipro.py
@@ -12,7 +12,7 @@ import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,
 
 vipro = LINETCR.LINE()
 #vipro.login(qr=True)
-vipro.login(token='EpJgyNvzK8xQp88I04t9.2PnOnQMgNRHUYkUzkK6jEq./p92mOc1BPmafZPSgTcw4mkpBeT/czsWHBLRbK7Aewc=')
+vipro.login(token='EuDS0BSgJxPYKOTcRlGb.MRy0VN9I89ewGlG16rZF2W.1Mo4S/hHowA10KUAyruqbX91H98mpIIAdi/p0vosbSw=')
 vipro.loginResult()
 print "Vipro-Login Success\n\n=====[Sukses Login]====="
 
